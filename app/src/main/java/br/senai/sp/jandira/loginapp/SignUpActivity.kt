238960265aece8df0b253960841b51ad9309d2d2
GUSTAVO SOUZA TENORIO DE BARROS <GustavoTenorio010@gmail.com>
2023-03-14 16:56:35 -0300

    Tela de SignUp

--- app/src/main/java/br/senai/sp/jandira/loginapp/SignUpActivity.kt
+++ app/src/main/java/br/senai/sp/jandira/loginapp/SignUpActivity.kt
@@ -6,12 +6,12 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import br.senai.sp.jandira.loginapp.ui.theme.LoginAPPTheme
@@ -21,11 +21,8 @@ class SignUpActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             LoginAPPTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                }
+                SignUpScreen()
+                
             }
         }
     }
@@ -34,7 +31,7 @@ class SignUpActivity : ComponentActivity() {
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
-fun DefaultPreview2() {
+fun SignUpScreen() {
     LoginAPPTheme {
         Surface(
             modifier = Modifier.fillMaxSize(),
@@ -56,7 +53,7 @@ fun DefaultPreview2() {
             Row(
                 horizontalArrangement = Arrangement.Center
             ) {
-                Text(text = "A")
+
             }
        }
     }
